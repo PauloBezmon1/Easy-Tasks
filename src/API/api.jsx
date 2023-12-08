5cdc76973530a3b0732f52c1836bfff76077f6cc
João Pedro Nishikawa Alves <72231778+joaonishikawa@users.noreply.github.com>
2023-12-08 13:12:18 -0300

    Add files via upload

--- src/API/api.jsx
+++ src/API/api.jsx
@@ -1,22 +1,34 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-//link da api: http://localhost:3000/
+// Custom Hook para Requisições de API
+const useAxios = (url) => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const headers = {
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1NzI1OGMxNmEyNTcxNjViZjQ4NjViNiIsImlhdCI6MTcwMTk5MjY2NiwiZXhwIjoxNzAyMDc5MDY2fQ.OOoey8gUcGpQrPHC8BxrYd-MC0oOtu5qCjcz7wxW3QY'
+        };
 
-const Data = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await axios.get(`http://localhost:3000/EasyTask/${url}`, { headers });
+        setData(response.data);
+        setError(null);
+      } catch (error) {
+        setError(error);
+        setData(null);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    const [data, setData] = useState([])
+    fetchData();
+  }, [url]);
 
-    useEffect (() => {
-        axios.get("http://localhost:3000/")
-          .then(async(res) => {
-                await setData(res.data)
-          })
-          .catch(err => console.log(err))
+  return { data, error, loading };
+};
 
-    }, [])
-
-    return data
-}
-
-export default Data
+export default useAxios;
--- src/components/tasks/MainTasks.jsx
+++ src/components/tasks/MainTasks.jsx
@@ -1,12 +1,46 @@
 import "./Header-Task.css"
 import Aside from "../tasks/Aside";
+import Data from "../../API/api"
+import react from "react"
+import { useState, useEffect } from "react"
+import useAxios from "../../API/api";
 
 function MainTasks() {
-    return ( 
+    const { data, error, loading } = useAxios("task")
+    const [Iniciar, setIniciar] = useState([]);
+    const [Produzindo, setProduzindo] = useState([]);
+    const [Finalizado, setFinalizado] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (data) {
+                const iniciarArray = [];
+                const produzindoArray = [];
+                const finalizadoArray = [];
+
+                data.forEach((element) => {
+                    if (element.status === "Iniciar") {
+                        iniciarArray.push(element);
+                    } else if (element.status === "Produzindo") {
+                        produzindoArray.push(element);
+                    } else if (element.status === "Finalizado") {
+                        finalizadoArray.push(element);
+                    }
+                });
+
+                setIniciar(iniciarArray);
+                setProduzindo(produzindoArray);
+                setFinalizado(finalizadoArray);
+            }
+        };
+
+        fetchData();
+    }, [data]);
+
+    return (
         <main className="flex w-full justify-end h-max">
             <div className="flex md:flex-row w-full flex-col mx-2 xl:mx-auto h-full personalized-height">
                 <Aside />
-
                 <div className="bg-gray-100 dark:bg-slate-950 h-max flex flex-col items-center xl:items-start flex-nowrap justify-center px-4 md:px-20 py-10 gap-14 xl:gap-7 w-full xl:flex-row rounded-r">
                     <div className="flex flex-col gap-0 justify-center">
                         <div className="flex gap-3 items-center"> {/* Header */}
@@ -14,68 +48,34 @@ function MainTasks() {
                             <p className="md:font-thin text-xs dark:text-white">Pending</p>
                         </div>
 
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
+                        {Iniciar.map((element, index) => (
+                            <div className="mt-5" key={index}> {/* Main */}
+                                <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
+                                    <h1 className="font-medium text-base md:text-xl dark:text-white">{element.titulo}</h1>
+                                    <p className="text-xs font-thin md:text-base text-justify dark:text-white">{element.tarefa}</p>
+                                    <p className="font-thin text-xs dark:text-white">{element.createdAt}</p>
+                                </div>
                             </div>
-                        </div>
+                        ))}
 
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
-                            </div>
-                        </div>
 
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
-                            </div>
-                        </div>
-
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
-                            </div>
-                        </div>
-
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
-                            </div>
-                        </div>
-
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 red rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
-                            </div>
-                        </div>
                     </div>
-                    
+
                     <div className="flex flex-col gap-0">
                         <div className="flex gap-3 items-center"> {/* Header */}
                             <div className="rounded-full bg-yellow-400 h-3.5 w-3.5"></div>
                             <p className="font-thin text-xs dark:text-white">Progress</p>
                         </div>
 
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 yellow rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
+                        {Produzindo.map((element, index) => (
+                            <div className="mt-5" key={index}> {/* Main */}
+                                <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 yellow rounded-md bg-white dark:bg-slate-900">
+                                    <h1 className="font-medium text-base md:text-xl dark:text-white">{element.titulo}</h1>
+                                    <p className="text-xs font-thin md:text-base text-justify dark:text-white">{element.tarefa}</p>
+                                    <p className="font-thin text-xs dark:text-white">{element.createdAt}</p>
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
 
                     <div className="flex flex-col gap-0">
@@ -84,13 +84,15 @@ function MainTasks() {
                             <p className="font-thin text-xs dark:text-white">Completed</p>
                         </div>
 
-                        <div className="mt-5"> {/* Main */}
-                            <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 green rounded-md bg-white dark:bg-slate-900">
-                                <h1 className="font-medium text-base md:text-xl dark:text-white">Lorem Ipsum</h1>
-                                <p className="text-xs font-thin md:text-base text-justify dark:text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim adipisci amet ex, officia porro qui eligendi repellat, sapiente et, totam harum facilis quibusdam hic temporibus odio non placeat nulla reiciendis.</p>
-                                <p className="font-thin text-xs dark:text-white">dd/mm/yyyy</p>
+                        {Finalizado.map((element, index) => (
+                            <div className="mt-5" key={index}> {/* Main */}
+                                <div className="flex flex-col min-w-[250px] md:min-w-[280px] max-w-xs p-6 gap-3 green rounded-md bg-white dark:bg-slate-900">
+                                    <h1 className="font-medium text-base md:text-xl dark:text-white">{element.titulo}</h1>
+                                    <p className="text-xs font-thin md:text-base text-justify dark:text-white">{element.tarefa}</p>
+                                    <p className="font-thin text-xs dark:text-white">{element.createdAt}</p>
+                                </div>
                             </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
